002b696a6608a976fb44775a8ac113fc998b7dc0
maksed4 <maxim.eyd@utb.ecutbildning.se>
2022-10-30 21:52:54 +0100

    byggt form utan css

---
+++ src/Components/Login.js
@@ -0,0 +1,53 @@
+import { useState, button } from "react";
+import LoginForm from "./LoginFrom";
+
+
+
+const Login = () => {
+
+    const adminUser = {
+        email: "admin@admin.com",
+        password: "admin1234"
+    }
+
+    const [user, Setuser] = useState({ username: "", email: "" });
+    const [error, Seterror] = useState("")
+
+
+
+    const Log = details => {
+        console.log(details)
+
+        if (details.email == adminUser.email && details.password == adminUser.password) {
+            console.log("Logged in")
+            Setuser({
+                name: details.username,
+                email: details.email
+            });
+        } else {
+            console.log("do not match")
+            Seterror("do not match")
+        }
+    }
+
+    const Logout = () => {
+        Setuser({ username: "", email: "" })
+    }
+
+    return (
+        <div className="App">
+            {(user.email != "") ? (
+                <div className="Welcome">
+                    <h2>Welcome, <span>{user.name}</span></h2>
+                    <button onClick={Logout}>Logout</button>
+                </div>
+            ) : (
+                <LoginForm Log={Log} error={error} />
+            )}
+        </div>
+    )
+
+}
+
+
+export default Login;
---
+++ src/Components/LoginFrom.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+const LoginForm = ({ Log, error }) => {
+
+    const [details, Setdetails] = useState({ username: "", email: "", password: "" })
+
+    const submitHandler = e => {
+        e.preventDefault();
+
+        Log(details);
+    }
+
+
+    return (
+        <form onSubmit={submitHandler}>
+            <div className="form-inner">
+                <h2> Login </h2>
+                <div className="form-group">
+                    <label htmlFor="username">Username:</label>
+                    <input
+                        type="text"
+                        name="Username"
+                        id="Username"
+                        onChange={e => Setdetails({ ...details, username: e.target.value })} value={details.username}></input>
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        type="text"
+                        name="email"
+                        id="email"
+                        onChange={e => Setdetails({ ...details, email: e.target.value })} value={details.email}></input>
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type="text"
+                        name="Password"
+                        id="Password"
+                        onChange={e => Setdetails({ ...details, password: e.target.value })} value={details.password} ></input>
+                </div>
+                <input type="submit" value="Login" />
+            </div>
+
+        </form>
+    )
+}
+export default LoginForm;
