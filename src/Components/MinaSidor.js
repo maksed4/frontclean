a0be08e964795b12b68d649a89814da843e60e8a
ingelaarrbrant <ingela.arrbrant@utb.ecutbildning.se>
2022-11-02 13:36:10 +0100

    Test

---
+++ src/Components/MinaSidor.js
@@ -0,0 +1,27 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom";
+import LoginForm from "./LoginForm";
+
+const MinaSidor = () => {
+
+    let navigate = useNavigate();
+
+
+    const [loggedInUser, SetLoggedInUser] = useState(null);
+
+
+    return (
+        <div>
+
+            <div>
+                <h1>Asalam alekym.</h1>
+            </div>
+
+
+        </div>
+    )
+
+}
+
+
+export default MinaSidor;
