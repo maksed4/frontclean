e6e05436063d398a2ad824f3509db8fc7d258336
Youssef Al Shadidi <youssef.as@gmail.com>
2022-11-15 18:22:24 +0100

    IN-18 refactored alternatives user see on cleaning types

--- src/Components/sub/CurrentCustomerSidor.js
+++ src/Components/sub/CurrentCustomerSidor.js
@@ -30,32 +30,35 @@ const CurrentCustomerSidor = () => {
     }, [])
 
     return (
-        <div>
-            <h1>Personlig information</h1>
-            <p>Förnamn: {loggedInCustomer.firstname}</p>
-            <p>Efternamn: {loggedInCustomer.lastname}</p>
-            <p>Adress: {loggedInCustomer.address}</p>
-            <p>Postkod: {loggedInCustomer.zipcode}</p>
-            <p>Stad: {loggedInCustomer.city}</p>
+        loggedInCustomer.customerType.includes("PRIVATE_CUSTOMER") ?
+            <div>
+                <h1>Personlig information</h1>
+                <p>Förnamn: {loggedInCustomer.firstname}</p>
+                <p>Efternamn: {loggedInCustomer.lastname}</p>
+                <p>Adress: {loggedInCustomer.address}</p>
+                <p>Postkod: {loggedInCustomer.zipcode}</p>
+                <p>Stad: {loggedInCustomer.city}</p>
 
-            <h1>Mina bokningar</h1>
-            {bookings.map(booking =>
-                <div key={booking.id}>
-                    <p>Datum: {booking.cleaningDate.substring(0, 10)}</p>
-                    <p>Tid: {booking.cleaningDate.substring(11, 16)}</p>
-                    <p>Plats: {booking.location}</p>
-                    <p>Typ: {
-                        booking.cleaningType.includes("TOP_CLEANING") ? <span>Top Cleaning</span> :
-                        booking.cleaningType.includes("DIAMOND_CLEANING") ? <span>Diamond Cleaning</span> :
-                        booking.cleaningType.includes("WINDOW_CLEANING") ? <span>Window Cleaning</span> :
-                        booking.cleaningType.includes("BASIC_CLEANING") ? <span>Basic Cleaning</span> :
-                                <></>
-                    }</p>
-                    <p>Färdig: {booking.done ? <span> Ja </span> : <Avboka id={booking.id} />}</p>
-                    <br/>
-                </div>
-            )}
-        </div>
+                <h1>Mina bokningar</h1>
+                {bookings.map(booking =>
+                    <div key={booking.id}>
+                        <p>Datum: {booking.cleaningDate.substring(0, 10)}</p>
+                        <p>Tid: {booking.cleaningDate.substring(11, 16)}</p>
+                        <p>Plats: {booking.location}</p>
+                        <p>Typ: {
+                            booking.cleaningType.includes("TOP_CLEANING") ? <span>Top</span> :
+                                booking.cleaningType.includes("DIAMOND_CLEANING") ? <span>Diamond</span> :
+                                    booking.cleaningType.includes("WINDOW_CLEANING") ? <span>Fönster</span> :
+                                        booking.cleaningType.includes("BASIC_CLEANING") ? <span>Basic</span> :
+                                            <></>
+                        }</p>
+                        <p>Färdig: {booking.done ? <span> Ja </span> : <Avboka id={booking.id} />}</p>
+                        <br/>
+                    </div>
+                )}
+            </div>
+            :
+            <></>
     )
 }
 
