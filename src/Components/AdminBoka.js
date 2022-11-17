8c35d451fbefdd49a2a9b87ab33fb3a8f91f56c7
Youssef Al Shadidi <youssef.as@gmail.com>
2022-11-17 23:42:22 +0100

    refactored some code so site works better

--- src/Components/AdminBoka.js
+++ src/Components/AdminBoka.js
@@ -1,6 +1,7 @@
 import {useEffect, useState} from "react";
 import axios from "axios";
 import AuthService from "../Services/AuthService";
+import {useNavigate} from "react-router-dom";
 
 const Boka = () => {
 
@@ -16,6 +17,8 @@ const Boka = () => {
         {value: 'BASIC_CLEANING', text: 'Basic'},
         {value: 'WINDOW_CLEANING', text: 'Window'}
     ];
+
+    let navigate = useNavigate();
 
     const handleCustomerId = (e) => {
         setCustomerId(e.target.value);
@@ -50,9 +53,12 @@ const Boka = () => {
                 'Content-Type': 'application/json',
                 'Authorization': 'Bearer ' + AuthService.getCurrentUser().token
             }
+        }).then(() => {
+            navigate("/cleanings");
+            window.location.reload();
+            alert("Städning bokad");
         })
         console.log(response);
-
     }
 
 
