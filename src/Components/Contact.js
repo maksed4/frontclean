b48bc95b75b36bda6ef33a3df674556844f20501
ingelaarrbrant <ingela.arrbrant@utb.ecutbildning.se>
2022-11-22 11:22:51 +0100

    Small changes

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,10 +1,23 @@
-
+import { Layout } from "./Layout";
+import Kontakt from "../images/Kontakt.png"
 
 const Contact = () => {
 
     return (
-        <div>
-            Hej
+        <div className="IndragBild">
+            <div className="IndragBildUser">
+                <img src={Kontakt} alt="Bild Kontakt" width="400" height="400"></img>
+            </div>
+            <div>
+                <h3 className="Rubrik" >Städa Fint AB</h3>
+                <p className="IndragKontakt">Adress: Mäster Samuelsgatan 45</p>
+                <p className="IndragKontakt">Stad: Stockholm</p>
+                <p className="IndragKontakt">Telefon: 08-800 800</p>
+                <p className="IndragKontakt">E-mail: info@stadafint.se</p>
+                <br></br>
+                <p className="IndragKontakt">Följ oss gärna på: </p>
+                <p></p>
+            </div>
         </div>
     );
 }
